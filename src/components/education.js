4ad8fb1c7a9d219afc1b4cc8fb818a857351a551
Jørgen <mail@jvik.no>
2018-09-10 16:53:14 +0200

    bugfix

--- src/components/education.js
+++ src/components/education.js
@@ -5,11 +5,11 @@ class Education extends Component {
   render() {
     return( 
       <Grid>
-        <Cell col={4}>
+        <Cell col={2}>
         <p>{this.props.startYear} - {this.props.endYear}</p>
         </Cell>
         
-        <Cell col={8}>
+        <Cell col={10}>
           <h4 style={{marginTop: '0px', marginBottom: '2px'}}>{this.props.schoolName}</h4>
           <h5 style={{marginTop: '0px', paddingTop: '0px'}}>{this.props.educationTitle}</h5>
           <p>{this.props.schoolDescription}</p>
