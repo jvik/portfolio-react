0ce14c6b697fc4cf5866ec4cbd81b1c2f5775a98
Jørgen V <mail@jvik.no>
2019-03-05 22:56:56 +0100

    Updates

---
+++ precache-manifest.0470e5daa6515ccaf40686b960df546c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e348197e6e412a7eb025ceb7fb08e12",
+    "url": "/portfolio-react/static/media/photo.8e348197.png"
+  },
+  {
+    "revision": "7bd9fe0f62718945cfc231beb65dd5d4",
+    "url": "/portfolio-react/static/media/nystudent.7bd9fe0f.png"
+  },
+  {
+    "revision": "780bcbc715c88212df47",
+    "url": "/portfolio-react/static/js/runtime~main.780bcbc7.js"
+  },
+  {
+    "revision": "621defe05ad9b9ad84d2",
+    "url": "/portfolio-react/static/js/main.621defe0.chunk.js"
+  },
+  {
+    "revision": "bfeaffd1f94d62e56a34",
+    "url": "/portfolio-react/static/js/2.bfeaffd1.chunk.js"
+  },
+  {
+    "revision": "621defe05ad9b9ad84d2",
+    "url": "/portfolio-react/static/css/main.dacae14b.chunk.css"
+  },
+  {
+    "revision": "bfeaffd1f94d62e56a34",
+    "url": "/portfolio-react/static/css/2.2331d0fd.chunk.css"
+  },
+  {
+    "revision": "a02f76b3ecddcbb5394b85e74943cec6",
+    "url": "/portfolio-react/index.html"
+  }
+];
