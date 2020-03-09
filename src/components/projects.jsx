41c8b288937405909dd2ccde26b7b84dbd0bb912
Jørgen <mail@jvik.no>
2020-03-09 20:32:59 +0100

    Minor fixes. Removed unneccesary component.

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,69 +1,66 @@
 import React, { Component } from 'react';
-import { Card, CardTitle, CardText, CardActions, Button } from 'react-mdl';
-import { Grid, Row } from 'react-flexbox-grid';
+import { Grid, Card, CardTitle, CardText, CardActions, Button } from 'react-mdl';
 
 class Projects extends Component {
 	render() {
 		return (
 			<div className="cardContainer">
 				<Grid>
-					<Row around="xs">
-						<Card className="projectCard" shadow={2} style={{ margin: '1em' }}>
-							<CardTitle>Norsk Coronavirus-statistikk</CardTitle>
-							<CardText style={{ height: '100%' }}>
-								<p>
-									PWA made with VueJS fetching statistics from various sources regarding infection
-									rates in Norway.
-								</p>
-							</CardText>
-							<CardActions border>
-								<a href="https://github.com/jvik/corona">
-									<Button colored>
-										<i className="fab fa-github" /> GitHub
-									</Button>
-								</a>
-								<a href="https://corona.jvik.no">
-									<Button colored>
-										<i className="fas fa-vial" /> Demo
-									</Button>
-								</a>
-							</CardActions>
-						</Card>
-						<Card className="projectCard" shadow={2} style={{ margin: '1em' }}>
-							<CardTitle>Intopro</CardTitle>
-							<CardText style={{ height: '100%' }}>
-								<p>
-									E-learning platform for food safety built with NodeJS backend and VueJS frontend.
-									Startup developed by Supernode, Eggedosis and ISO-Piloten.
-								</p>
-								<p>No demo available. Licenses can be purchased.</p>
-							</CardText>
-							<CardActions border>
-								<a href="https://intopro.no">
-									<Button colored>
-										<i className="fas fa-building" /> Company website
-									</Button>
-								</a>
-							</CardActions>
-						</Card>
-						<Card className="projectCard" shadow={2} style={{ margin: '1em' }}>
-							<CardTitle>INFO134 Prosjekt</CardTitle>
-							<CardText style={{ height: '100%' }}>
-								<p>
-									Webpage built second year of university. Mobile first approach with flexbox & grid.
-									Semantic tags in HTML5. API integration towards data.norge.no to fetch and process
-									information from external sources. Built with HTML5, CSS3 and vanilla JS.
-								</p>
-							</CardText>
-							<CardActions border>
-								<a href="https://github.com/jvik/INFO134-Semesteroppgave-Nystudent">
-									<Button colored>
-										<i className="fab fa-github" /> GitHub
-									</Button>
-								</a>
-							</CardActions>
-						</Card>
-					</Row>
+					<Card className="projectCard" shadow={2} style={{ margin: '1em' }}>
+						<CardTitle style={{ maginTop: '0.1em' }}>Norsk Coronavirus-statistikk</CardTitle>
+						<CardText style={{ height: '100%' }}>
+							<p>
+								PWA made with VueJS fetching statistics from various sources regarding COVID-19
+								infection rates in Norway.
+							</p>
+						</CardText>
+						<CardActions border>
+							<a href="https://github.com/jvik/corona">
+								<Button colored>
+									<i className="fab fa-github" /> GitHub
+								</Button>
+							</a>
+							<a href="https://corona.jvik.no">
+								<Button colored>
+									<i className="fas fa-vial" /> Demo
+								</Button>
+							</a>
+						</CardActions>
+					</Card>
+					<Card className="projectCard" shadow={2} style={{ margin: '1em' }}>
+						<CardTitle>Intopro</CardTitle>
+						<CardText style={{ height: '100%' }}>
+							<p>
+								E-learning platform for food safety built with NodeJS backend and VueJS frontend.
+								Startup developed by Supernode, Eggedosis and ISO-Piloten.
+							</p>
+							<p>No demo available. Licenses can be purchased.</p>
+						</CardText>
+						<CardActions border>
+							<a href="https://intopro.no">
+								<Button colored>
+									<i className="fas fa-building" /> Company website
+								</Button>
+							</a>
+						</CardActions>
+					</Card>
+					<Card className="projectCard" shadow={2} style={{ margin: '1em' }}>
+						<CardTitle>INFO134 Prosjekt</CardTitle>
+						<CardText style={{ height: '100%' }}>
+							<p>
+								Webpage built second year of university. Mobile first approach with flexbox & grid.
+								Semantic tags in HTML5. API integration towards data.norge.no to fetch and process
+								information from external sources. Built with HTML5, CSS3 and vanilla JS.
+							</p>
+						</CardText>
+						<CardActions border>
+							<a href="https://github.com/jvik/INFO134-Semesteroppgave-Nystudent">
+								<Button colored>
+									<i className="fab fa-github" /> GitHub
+								</Button>
+							</a>
+						</CardActions>
+					</Card>
 				</Grid>
 			</div>
 		);
